27d46813f56c9014d8b6ad2183273b9927f730c0
Valentin Granados <valefcfhc@gmail.com>
2015-10-23 20:36:26 -0500

    Initial Commit

---
+++ FacebookMatchingFriendsSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FacebookMatchingFriendsSwift
+//
+//  Created by Emmanuel Valentín Granados López on 23/10/15.
+//  Copyright © 2015 DevWorms. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
