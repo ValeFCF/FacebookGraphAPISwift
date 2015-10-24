8fe60543cbcccc711241906a9e756d571bc2e7d6
Valentin Granados <valefcfhc@gmail.com>
2015-10-23 22:57:48 -0500

    Adding FBSDKLoginButtonDelegate

--- FacebookMatchingFriendsSwift/ViewController.swift
+++ FacebookMatchingFriendsSwift/ViewController.swift
@@ -10,14 +10,23 @@ import UIKit
 import FBSDKCoreKit
 import FBSDKLoginKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, FBSDKLoginButtonDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        if ((FBSDKAccessToken.currentAccessToken()) == nil){
+            print("Not logged in..")
+        }
+        else{
+            print("Logged in..")
+        }
+        
         let loginButton = FBSDKLoginButton()
+        loginButton.delegate = self //important!
         loginButton.center = self.view.center
+        loginButton.readPermissions =  ["public_profile", "email", "user_friends"]
         self.view.addSubview(loginButton)
         
     }
@@ -26,6 +35,19 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //Mark - FBSDKLoginButtonDelegate
+    
+    func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
+
+        
+        
+    }
+    
+    func loginButtonDidLogOut(loginButton: FBSDKLoginButton!) {
+        print("logOut")
+    }
+
 
 
 }
