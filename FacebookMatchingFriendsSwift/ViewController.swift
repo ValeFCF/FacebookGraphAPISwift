1c72fa5739bf29168df2e38fc514eff2a146b36f
Valentin Granados <valefcfhc@gmail.com>
2015-10-28 22:26:47 -0600

    FBSDKGameRequestDialog

--- FacebookMatchingFriendsSwift/ViewController.swift
+++ FacebookMatchingFriendsSwift/ViewController.swift
@@ -9,8 +9,9 @@
 import UIKit
 import FBSDKCoreKit
 import FBSDKLoginKit
+import FBSDKShareKit
 
-class ViewController: UIViewController, FBSDKLoginButtonDelegate {
+class ViewController: UIViewController, FBSDKLoginButtonDelegate, FBSDKGameRequestDialogDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,21 +23,48 @@ class ViewController: UIViewController, FBSDKLoginButtonDelegate {
         else{
             print("Logged in..")
             
-           FBSDKGraphRequest.init(graphPath: "me", parameters: ["fields": "id, name, picture.type(large)" ] ).startWithCompletionHandler({ (connection, result, error) -> Void in
+           /*FBSDKGraphRequest.init(graphPath: "me", parameters: ["fields": "id, name, picture.type(large)" ] ).startWithCompletionHandler({ (connection, result, error) -> Void in
                 //picture.type(large)
                 if error == nil {
                     print(result);
                 }
                 
-            })
+            })*/
             
-            FBSDKGraphRequest.init(graphPath: "me/friends", parameters: ["fields": "id, name, picture{url}", "limit": "5"] ).startWithCompletionHandler({ (connection, result, error) -> Void in
+           /* FBSDKGraphRequest.init(graphPath: "me/friends", parameters: ["fields": "id, name, picture{url}", "limit": "5"] ).startWithCompletionHandler({ (connection, result, error) -> Void in
                 
                 if error == nil {
                     print(result);
                 }
                 
+            })*/
+            
+            FBSDKGraphRequest.init(graphPath: "me/friends", parameters: ["fields": "name, id"] ).startWithCompletionHandler({ (connection, result, error) -> Void in
+                
+                if error == nil {
+                    
+                    let data = result as! NSDictionary
+                    let data3 = data["data"]![0]["id"] as AnyObject
+
+                    print( data3 )
+                    
+                    let gameRequestContent = FBSDKGameRequestContent()
+                    gameRequestContent.message = "YOUR_MESSAGE_HERE";
+                    gameRequestContent.title = "OPTIONAL TITLE";
+                    gameRequestContent.recipients = [ data3 ]
+                    
+                    // Assuming self implements <FBSDKGameRequestDialogDelegate>
+                    FBSDKGameRequestDialog.showWithContent(gameRequestContent, delegate: self)
+                    
+                    
+                } else {
+                    print("Error: \(error.localizedDescription)")
+                }
+                
             })
+
+            
+    
             
             
             
@@ -84,6 +112,24 @@ class ViewController: UIViewController, FBSDKLoginButtonDelegate {
     }
 
 
+    //Mark - FBSDKGameRequestDialogDelegate
+    
+    func gameRequestDialog(gameRequestDialog: FBSDKGameRequestDialog!, didCompleteWithResults results: [NSObject : AnyObject]!) {
+        print("didCompleteWithResults:")
+        print(results)
+    }
+    
+    func gameRequestDialog(gameRequestDialog: FBSDKGameRequestDialog!, didFailWithError error: NSError!) {
+        print("didFailWithError")
+    }
+    
+    func gameRequestDialogDidCancel(gameRequestDialog: FBSDKGameRequestDialog!) {
+        print("gameRequestDialogDidCancel")
+    }
+    
+    
+    
+    
 
 }
 
